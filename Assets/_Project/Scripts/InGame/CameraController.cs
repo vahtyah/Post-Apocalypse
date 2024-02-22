9bcb6954ed49518af4e6f89d8802b7d7d7013d11
Туан <8268826@gmail.com>
2024-02-22 13:23:17 +0300

    setup camera

---
+++ Assets/_Project/Scripts/InGame/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Vector3 distance;
+    [SerializeField] private Transform playerTransform;
+    //[SerializeField] float smoothTime;
+    //Vector3 curVelo;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - playerTransform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 targetPos = playerTransform.position + offset;
+        transform.position = targetPos;
+        //transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref curVelo, smoothTime, Mathf.Infinity, Time.deltaTime) ;
+    }
+}
